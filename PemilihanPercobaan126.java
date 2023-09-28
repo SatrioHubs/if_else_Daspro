5fb63cc64fc0d58164446e2b432c34f4a7a92ec6
SatrioHubs <satrioramadhani387@gmail.com>
2023-09-28 19:45:28 +0700

    push commit Tenary operator

--- PemilihanPercobaan126.java
+++ PemilihanPercobaan126.java
@@ -7,15 +7,19 @@ import java.util.Scanner;
 public class PemilihanPercobaan126 {
 
     public static void main(String[] args) {
-        Scanner input26 = new Scanner(System.in);
+        // Scanner input26 = new Scanner(System.in);
 
-        System.out.println("masukan angka: ");
-        int angka = input26.nextInt();
+        // System.out.println("masukan angka: ");
+        // int angka = input26.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka "+angka+" bilangan genap");
-        }else{
-            System.out.println("Angka "+angka+" bilangan ganjil");
-        }
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka "+angka+" bilangan genap");
+        // }else{
+        //     System.out.println("Angka "+angka+" bilangan ganjil");
+        // }
+        int angka = 7;
+        String hasil;
+        hasil = (angka % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
+        System.out.println(angka + " adalah " + hasil);
     }
 }
