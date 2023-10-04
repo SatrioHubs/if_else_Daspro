d6d3b5749f9600a21364f047c07b86ad76cf3e03
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-04 19:50:22 +0700

    memunculkan 2100

--- Pemilihan2Percobaan126.java
+++ Pemilihan2Percobaan126.java
@@ -11,8 +11,11 @@ public class Pemilihan2Percobaan126 {
         tahun = input26.nextInt();
         
         if ((tahun  % 4) == 0) {
-            if ((tahun % 100) != 0) 
-                System.out.println("Tahun Kabisat");   
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            } else{
+                System.out.println("Bukan Tahun Kabisat");
+            }   
         } else{
             System.out.println("Bukan Tahun Kabisat");
         }       
