7a6df198f166d2d9de31c122b5c17dff36069cad
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-23 08:21:00 +0700

    ganti equals

--- Pemilihan2Percobaan326.java
+++ Pemilihan2Percobaan326.java
@@ -15,7 +15,7 @@ public class Pemilihan2Percobaan326 {
         System.out.println("Masukan Besarnya Penghasilan");
         penghasilan = input26.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000) 
                 pajak = 0.1;
             else if(penghasilan <= 3500000)
@@ -24,7 +24,7 @@ public class Pemilihan2Percobaan326 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("penghasilan bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
